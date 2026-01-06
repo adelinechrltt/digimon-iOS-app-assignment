a3c5dd5afd3f27f5fbcbd35c01f565d527643e63
Adeline Charlotte <adeline.chrltt@gmail.com>
2026-01-06 22:55:11 +0700

    Initial commit

---
+++ DigimonAssignment/DigimonAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigimonAssignmentApp.swift
+//  DigimonAssignment
+//
+//  Created by Adeline Charlotte Augustinne on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct DigimonAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DigimonAssignmentTests/DigimonAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  DigimonAssignmentTests.swift
+//  DigimonAssignmentTests
+//
+//  Created by Adeline Charlotte Augustinne on 06/01/26.
+//
+
+import Testing
+@testable import DigimonAssignment
+
+struct DigimonAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
