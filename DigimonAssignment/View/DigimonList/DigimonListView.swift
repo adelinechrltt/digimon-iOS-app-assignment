04383bfb32734a7a4e4c04c33e5b93cda184da0c
Adeline Charlotte <adeline.chrltt@gmail.com>
2026-01-08 16:23:59 +0700

    feat: Integrate SearchBar to ListView layout

--- DigimonAssignment/View/DigimonList/DigimonListView.swift
+++ DigimonAssignment/View/DigimonList/DigimonListView.swift
@@ -13,44 +13,48 @@ struct DigimonListView: View {
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVGrid(
-                    columns:
-                        [
-                            GridItem(.flexible()),
-                            GridItem(.flexible())
-                        ],
-                    spacing: 10) {
-                        ForEach(Array(vm.digimons.enumerated()), id: \.offset) { index, digimon in
-                        NavigationLink {
-                            DigimonCard(
-                                cardSize: .expanded,
-                                digimon: digimon)
-                        } label: {
-                            DigimonCard(
-                                cardSize: .minimized,
-                                digimon: digimon)
+            VStack {
+                SearchBar()
+                ScrollView {
+                    LazyVGrid(
+                        columns:
+                            [
+                                GridItem(.flexible()),
+                                GridItem(.flexible())
+                            ],
+                        spacing: 10) {
+                            ForEach(Array(vm.digimons.enumerated()), id: \.offset) { index, digimon in
+                                NavigationLink {
+                                    DigimonCard(
+                                        cardSize: .expanded,
+                                        digimon: digimon)
+                                } label: {
+                                    DigimonCard(
+                                        cardSize: .minimized,
+                                        digimon: digimon)
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 10)
+                    if vm.page <= 1 {
+                        Text("Scroll to view more entries")
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                            .padding(20)
+                    }
+                }
+                .navigationTitle("Digimon")
+                .simultaneousGesture(
+                    DragGesture().onEnded { value in
+                        if value.translation.height < -50 {
+                            vm.loadNextPage()
                         }
                     }
+                )
+                .onAppear {
+                    if vm.digimons.isEmpty { vm.loadNextPage() }
                 }
-                .padding(.horizontal, 10)
-                if vm.page <= 1 {
-                    Text("Scroll to view more entries")
-                        .font(.subheadline)
-                        .foregroundStyle(.secondary)
-                        .padding(20)
-                }
-            }
-            .navigationTitle("Digimon")
-            .simultaneousGesture(
-                DragGesture().onEnded { value in
-                    if value.translation.height < -50 {
-                        vm.loadNextPage()
-                    }
-                }
-            )
-            .onAppear {
-                if vm.digimons.isEmpty { vm.loadNextPage() }
+
             }
         }
     }
