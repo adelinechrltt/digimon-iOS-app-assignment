c4557b3d42424b904cb62b4025ca3332893dc077
Adeline Charlotte <adeline.chrltt@gmail.com>
2026-01-08 21:33:09 +0700

    feat: Create models of metadata entities
    Create Attribute, TypeEntity, Level, and Field models

---
+++ DigimonAssignment/Model/Attribute.swift
@@ -0,0 +1,22 @@
+//
+//  AttributeEntity.swift
+//  DigimonAssignment
+//
+//  Created by Adeline Charlotte Augustinne on 08/01/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Attribute {
+    @Attribute(.unique) var id: Int
+    var name: String
+    var desc: String
+    
+    init(id: Int, name: String, desc: String) {
+        self.id = id
+        self.name = name
+        self.desc = desc
+    }
+}
