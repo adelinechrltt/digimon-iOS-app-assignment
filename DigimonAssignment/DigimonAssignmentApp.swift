ac74f49bf60fc5d1b241c116432deea006073312
Adeline Charlotte <adeline.chrltt@gmail.com>
2026-01-08 10:30:26 +0700

    feat: Inject ModelContext to app

--- DigimonAssignment/DigimonAssignmentApp.swift
+++ DigimonAssignment/DigimonAssignmentApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct DigimonAssignmentApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Digimon.self)
         }
     }
 }
